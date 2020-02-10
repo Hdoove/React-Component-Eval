bca45c139fc1c8bd98b5b0b20abcb7c57d6fc871
LiuDaDa <15302023722@163.com>
2020-02-10 18:52:08 +0800

    基本方式

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import Parent1 from './父子组件';
+// import Parent2 from './兄弟组件1';
+// import Parent3 from './兄弟组件2.1';
+// import Parent4 from './兄弟组件2.2';
+// import Parent5 from './兄弟组件2.3';
+import Parent6 from './兄弟组件3';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Parent1 /> */}
+      {/* <Parent2 /> */}
+      {/* <Parent3 /> */}
+      {/* <Parent4 /> */}
+      {/* <Parent5 /> */}
+      <Parent6 />
     </div>
   );
 }
